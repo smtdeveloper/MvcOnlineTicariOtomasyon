519ecbc8cf0afd285786d11d4f4be09e6ec20c7d
Samet Akca <developer2@cevizsoft.com>
2022-11-06 20:23:31 +0300

    GALERİ

---
+++ Controllers/GaleriController.cs
@@ -0,0 +1,24 @@
+﻿using MvcOnlineTicariOtomasyon.Models.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class GaleriController : Controller
+    {
+        Context c = new Context();
+       
+
+        // GET: Galeri
+        public ActionResult Index()
+        {
+
+            var values = c.Products.Where(x=> x.Status == true).ToList();
+            return View(values);
+
+        }
+    }
+}
