60c8a168828e5541360b1d2fd6c7ddec75c57899
Samet Akca <developer2@cevizsoft.com>
2022-11-22 00:40:56 +0300

    register

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,31 @@
+﻿using MvcOnlineTicariOtomasyon.Models.DB;
+using MvcOnlineTicariOtomasyon.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class RegisterController : Controller
+    {
+        Context c = new Context();
+
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CariRegister(Concubine concubine)
+        {
+            c.Concubines.Add(concubine);
+            c.SaveChanges();
+            return RedirectToAction("Index", "Login");
+
+        }
+
+    }
+}
