b385a4651719e377584e231923775d1bbd1bab61
Samet Akca <developer2@cevizsoft.com>
2022-12-03 19:15:25 +0300

    Grafikler

---
+++ Controllers/GrafikController.cs
@@ -0,0 +1,39 @@
+﻿using MvcOnlineTicariOtomasyon.Models;
+using MvcOnlineTicariOtomasyon.Models.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class GrafikController : Controller
+    {
+        // GET: Grafik
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult VisualizeProductResult()
+        {
+            return Json(ProductList(), JsonRequestBehavior.AllowGet);
+        }
+
+         public List<PoductStokModel> ProductList()
+        {
+            List<PoductStokModel> model = new List<PoductStokModel>();
+            using (var c = new Context())
+            {
+                model = c.Products.Select(x => new PoductStokModel
+                {
+                    name = x.ProductName,
+                    stok = x.Stok
+                }).ToList();
+            }
+            return model;
+        }
+
+    }
+}
---
+++ Models/PoductStokModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models
+{
+    public class PoductStokModel
+    {
+        public string name { get; set; }
+        public short stok { get; set; }
+    }
+}
